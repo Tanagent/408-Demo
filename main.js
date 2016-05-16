3d1a221bbafd6bce0562a571236c67cfca9017de
ChoiByungWook <dchoi1@cpp.edu>
2016-05-16 00:59:29 -0700

    jesus

---
+++ main.js
@@ -0,0 +1,34 @@
+// window.onload = function() {
+	// var button = document.querySelector('.sorting');
+// 	// var div = document.querySelector('unsorted');
+// 	//Click button
+// 	//Update the div, with a sorted version
+
+// 	//sort 3 1 4 2 5 -> 1 2 3 4 5
+// 	//update div unsorted with 1 2 3 4 5
+// 	function sort(unsorted) {
+// 		return unsorted.sort().toString();
+// 	}
+
+// 	function getId() {
+// 		return document.getElementById("numbers").innerHTML.split(" ");
+// 	}
+
+$( "#sort" ).click(function() {
+	alert( "Handler for .click() called." );
+});
+	// $('sort').on('click', function (e) {
+	//  windows.alert("ass") })
+	// document.getElementById("sort").on(click,function(event) console.log("ass"))
+	//  = function(event) {
+	// 	console.log("ass");
+	// 	if($(div).hasClass('unsorted')) {
+	// 		var temp = sort(getId());
+	// 		document.getElementById("numbers").innerHTML = temp;
+	// 		event.preventDefault();
+	// 	}
+	// }
+// };
+
+// $('sort').on('click', function (e) {
+// alert("ass");
